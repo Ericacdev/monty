b7d15ae14c18ecc9f42e49b43612848d6d2379ac
Ericacdev <nechipato@gmail.com>
2023-12-15 11:50:04 -0800

    update

--- main.h
+++ main.h
@@ -23,6 +23,22 @@ struct stack_s *prev;
 struct stack_s *next;
 } stack_t;
 /**
+* struct bus_s - variables -args, file, line content
+* @arg: value
+* @file: pointer
+* @content: line content
+* @lifi: flag change stack <-> queue
+* Description: values carried
+*/
+typedef struct bus_s
+{
+char *arg;
+FILE *file;
+char *content;
+int lifi;
+} bus_t;
+extern bus_t bus;
+/**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
@@ -35,24 +51,6 @@ typedef struct instruction_s
 char *opcode;
 void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-
-
-/**
- * struct bus_s - variables -args, file, line content
- * @arg: value
- * @file: pointer
- * @content: line content
- * @lifi: flag change stack <-> queue
- * Description: values carried
- */
-typedef struct bus_s
-{
-char *arg;
-FILE *file;
-char *content;
-int lifi;
-} bus_t;
-extern bus_t bus;
 
 void op_pop(stack_t **head, unsigned int number);
 void op_pall(stack_t **head, unsigned int number);
